7ff0c3041e8faf077a6f524681600f60a9f1e2fc
Toriqul2023 <toriqulislam296@yahoo.com>
2025-11-06 21:17:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient("mongodb+srv://love:etLerl4FIL7YyRB5@cluster0.uaiap.mongodb.net/?appName=Cluster0");
+let db;
+
+// ✅ Connect to MongoDB
+ async function run(){ 
+   
+    try{
+      await client.connect()
+        const database=client.db('heart')
+        const collection= database.collection('love')
+        app.post('/save-love',async(req,res)=>{
+        const {YourName,PartnerName,number}=req.body
+         const result = await collection.insertOne({YourName,PartnerName,number});
+          res.send(result)
+          console.log(result);
+          
+        })
+        
+       app.post('/portfolio',async(req,res)=>{
+        const data=req.body
+        
+        res.send(result)
+       })
+    
+          
+    }
+    finally{
+      
+    }
+  
+  }
+run();
+
+
+
+
+app.listen(5000, () => console.log("🚀 Server running on port 5000"));
